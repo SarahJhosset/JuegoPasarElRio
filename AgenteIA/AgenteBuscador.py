06d93102d33fde1c7990b9f7a1b3beb17847f6ea
Sarah Cazorla <sarah.cazorla@ucb.edu.bo>
2026-08-24 17:05:04 -0400

    Codigo base

---
+++ AgenteIA/AgenteBuscador.py
@@ -0,0 +1,78 @@
+# **********************************************************
+# * Clase: Agente buscador                                 *
+# * Autor: Victor Estevez                                  *
+# * Version: v2023.03.29                                   *
+# * Descripcion: Implementacion de algoritmos de busqueda  *
+# *              sin informacion y con informacion         *
+# **********************************************************
+
+from AgenteIA.Agente import Agente
+from copy import deepcopy
+import time
+
+
+class AgenteBuscador(Agente):
+    def __init__(self):
+        Agente.__init__(self)
+        self.__estado_inicial = None
+        self.__estado_meta = None
+        self.__funcion_sucesor = []
+        self.__tecnica = None
+
+    def set_estado_inicial(self, e0):
+        self.__estado_inicial = e0
+
+    def set_estado_meta(self, ef):
+        self.__estado_meta = ef
+
+    def get_estado_inicial(self):
+        return self.__estado_inicial
+
+    def get_estado_meta(self):
+        return self.__estado_meta
+
+    def set_tecnica(self, t):
+        self.__tecnica = t
+
+    def add_funcion(self, f):
+        self.__funcion_sucesor.append(f)
+
+    def test_objetivo(self, e):
+        return e == self.__estado_meta
+
+    def generar_hijos(self, e):
+        hijos = []
+        for fun in self.__funcion_sucesor:
+            h = fun(e)
+            hijos.append(h)
+        return hijos
+
+    def mide_tiempo(funcion):
+        def funcion_medida(*args, **kwards):
+            inicio = time.time()
+            c = funcion(*args, **kwards)
+            print("Tiempo de ejecucion: ", time.time()-inicio)
+            return c
+        return funcion_medida
+
+    @mide_tiempo
+    def programa(self):
+        frontera = [[self.__estado_inicial]]
+        visitados = []
+        while frontera:
+            if self.__tecnica == "profundidad":
+                camino = frontera.pop()
+            else:
+                camino = frontera.pop(0)
+            nodo = camino[-1]
+            visitados.append(nodo)
+            if self.test_objetivo(nodo):
+                self.set_acciones(camino)
+                break
+            else:
+                for hijo in self.generar_hijos(nodo):
+                    if hijo not in visitados:
+                        aux = deepcopy(camino)
+                        aux.append(hijo)
+                        frontera.append(aux)
+           
---
+++ AgenteMapu.py
@@ -0,0 +1,23 @@
+from AgenteIA.AgenteBuscador import AgenteBuscador
+
+
+class AgenteMapu(AgenteBuscador):
+
+    def __init__(self):
+        AgenteBuscador.__init__(self)
+        # aqui tu codigo para la funciona sucesor
+        # setear el estado inicial    def programa(self):
+        # self.acciones = ["Aun no se implementó mi programa agente", "Debes codificar aqui"]
+
+    # setear el estado meta, etc
+    def pasa_aa(self, e):
+        # aqui tu codigo
+        pass
+
+
+    # una vez configurados bien los atributos de esta clase,
+    # deberiamos borrar las siguiente lineas, es decir dejar
+    # que el programa implementado en el agente busqueda
+    # resuelva el problema y devuelva las acciones....
+    def programa(self):
+        self.acciones = ["Aun no se implementó mi programa agente", "Debes codificar aqui"]
